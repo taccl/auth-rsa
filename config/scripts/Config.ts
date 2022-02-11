18915040cefa00dbe7029b62bad800f6d6bdeca2
TaYaKi71751 <shantian.tw@gmail.com>
2022-02-11 19:09:31 +0900

    Create scripts config

---
+++ config/scripts/Config.ts
@@ -0,0 +1,17 @@
+export const scripts = {
+	config: {
+		scripts: 'ts-node ./config/scripts'
+	},
+	lint: {
+		fix: 'eslint --fix --ext .ts,.tsx .',
+		run: 'eslint --ext .ts,.tsx .'
+	},
+	build: {
+		tsc: {
+			clean: 'tsc --build --clean && tsc --build',
+			clear: 'tsc --build --clean',
+			noargs: 'tsc',
+			version: 'tsc --version'
+		}
+	}
+};
